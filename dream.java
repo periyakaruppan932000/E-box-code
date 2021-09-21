0400b4d26113920d897f35e19526b7b0773c3606
periyakaruppan932000 <91140743+periyakaruppan932000@users.noreply.github.com>
2021-09-21 20:43:01 +0530

    Add files via upload

---
+++ dream.java
@@ -0,0 +1,15 @@
+
+package com.example;
+import java.util.*;
+
+public class dream {
+	public static void main(String[] args) {
+		        int [] arr=new int[]{12,17,19,20,21};
+		        int sum=0;
+		        for(int i=0;i<arr.length;i++)
+		        {
+		            sum=sum+arr[i];
+		        }
+		        System.out.println("The sum of elements is"+sum); 
+		        }
+		}
