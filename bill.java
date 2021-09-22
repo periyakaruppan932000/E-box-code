c67fff021b915c39a15a23cf517bb440309442cd
periyakaruppan932000 <91140743+periyakaruppan932000@users.noreply.github.com>
2021-09-22 13:46:16 +0530

    Add files via upload

---
+++ bill.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class bill {
+	    public static void main(String[] args) {
+	        int billno=110;
+	        int date=10;
+	        if(billno%100==date||billno%10==date&&billno%date==0)
+	        {
+	        System.out.println("hurray!! your lucky for day"); 
+	        }
+	        else 
+	        {
+	        System.out.println("oops,better luck next time!");   
+	        }
+	}
+	}
+
+
