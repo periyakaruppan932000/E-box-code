d233ae5d1087c60937748dd601671529ae98a8fb
periyakaruppan932000 <91140743+periyakaruppan932000@users.noreply.github.com>
2021-09-23 18:56:34 +0530

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class odd {
+
+	public static void main(String[] args) {
+		int [] arr=new int[]{12,17,19,20,21};
+        for(int i=1;i<arr.length;i=i+2)
+        {
+        System.out.println(arr[i]); 
+        }
+    }
+}
+
+
+	
+
+
