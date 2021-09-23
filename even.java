6e16ebf45d50a2d30e3f0854cb97b0012855a47b
periyakaruppan932000 <91140743+periyakaruppan932000@users.noreply.github.com>
2021-09-23 18:55:45 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class even {
+
+	public static void main(String[] args) {
+		int [] arr=new int[]{12,17,19,20,21};
+        for(int i=0;i<arr.length;i=i+2)
+        {
+        System.out.println(arr[i]); 
+        }
+    }
+}
