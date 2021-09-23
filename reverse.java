453d8d2947ddee7d0e3ae2ced7e6cfe17ea1d9e3
periyakaruppan932000 <91140743+periyakaruppan932000@users.noreply.github.com>
2021-09-23 18:57:23 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,15 @@
+package com.example;
+import java.util.*;
+
+public class reverse {
+
+	public static void main(String[] args) {
+	        int [] arr=new int[]{12,17,19,20,21};
+	        for(int i=arr.length-1;i>=0;i--)
+	        {
+	        System.out.println("The reverse elements are:"+arr[i]); 
+	        }
+	    }
+}
+
+
